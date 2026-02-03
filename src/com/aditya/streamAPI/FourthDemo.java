5ac70a62199ee422a9ab70d04ecf05eb19ab3a27
aditya <adityajain773568@gmail.com>
2026-02-03 11:31:48 +0530

    ForEach method demo -> Terminal Ops

--- src/com/aditya/streamAPI/FourthDemo.java
+++ src/com/aditya/streamAPI/FourthDemo.java
@@ -1,4 +1,28 @@
 package com.aditya.streamAPI;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+//forEach Method
 public class FourthDemo {
+
+
+    public static void main(String[] args) {
+        List<String> javaCourses = Arrays.asList("Core javaCourses" , "Advanced Java" , "Spring boot");
+        List<String> uiCourses = Arrays.asList("HTML" , "CSS" , "Bootstrap" , "JavaScript");
+
+        List<List<String>> courses = Arrays.asList(javaCourses, uiCourses);
+
+        courses.stream().forEach(System.out::println);
+
+        Stream<String> stringStream = courses.stream().flatMap(s -> s.stream());
+
+        stringStream.forEach(System.out::println);
+
+    }
+
+
+
 }
